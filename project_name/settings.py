410ebce5253c2e4f65b1dbdacdf34f0d2eba488b
Kenneth Reitz <me@kennethreitz.org>
2017-03-16 13:05:04 -0400

    fix flango

--- project_name/settings.py
+++ project_name/settings.py
@@ -133,3 +133,4 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_STATIC_PREFIX = '/'
